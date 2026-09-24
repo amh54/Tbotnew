8c2309f666f30eaab0983d5853d0d82b233ed363
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2026-09-24 08:48:43 -0400

    test

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,13 +3,17 @@ const { handleHangmanModal } = require("../handlers/hangmanHandler.js");
 const { handleSlashCommand } = require("../handlers/slashCommandHandler.js");
 const { handleMessageComponent } = require("../handlers/messageComponentHandler.js");
 const { handleAutocomplete } = require("../handlers/autocompleteHandler.js");
-
+const BANNED_USERS = new Set(["793911165469458432"])
 module.exports = {
   name: "interactionCreate",
   async run(interaction) {
     const client = interaction.client;
     const db = require("../../index.js");
-
+if (BANNED_USERS.has(interaction.user.id)) {
+      if (interaction.isAutocomplete()) {
+        return;
+      }
+    }
     if (interaction.type === InteractionType.ModalSubmit) {
       if (interaction.customId.startsWith("hangman-")) {
         return await handleHangmanModal(interaction);
